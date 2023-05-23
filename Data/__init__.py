7b92c8222537e27aca63ddb035d54ac0a0a6a5a6
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2023-05-23 23:23:22 +0200

    Clean up imports

--- Data/__init__.py
+++ Data/__init__.py
@@ -1,20 +1,12 @@
 from Data.CT_ssl import CTSSLDataModule
-from Data.CityScapes_ssl import CityScapesSSLDataModule
 from Data.MSD_ssl import MSDSSLDataModule
-from Data.NTNU import NTNUDatamodule
 from .MSD import MSDDataModule
 from .BTCV import BTCVDataModule
 from .BTCV_ssl import BTCVSSSLDataModule
-from .CityScapes import CustomCityScapesDataModule
-from .Pannuke import PannukeDataModule
 from .Brats import BratsDataModule
 from .Brats_ssl import BraTSSSLDataModule
 
 data_modules = {
-    "CityScapes": CustomCityScapesDataModule,
-    "CityScapesCat": CustomCityScapesDataModule,
-    "CityScapes-SSL": CityScapesSSLDataModule,
-    "Pannuke": PannukeDataModule,
     "BraTS": BratsDataModule,
     "BraTS-SSL": BraTSSSLDataModule,
     "BTCV": BTCVDataModule,
@@ -22,6 +14,4 @@ data_modules = {
     "MSD": MSDDataModule,
     "MSD-SSL": MSDSSLDataModule,
     "CT-SSL": CTSSLDataModule,
-    "NTNU": NTNUDatamodule,
-    "NTNUCat": NTNUDatamodule,
 }
